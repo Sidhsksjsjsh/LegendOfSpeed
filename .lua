3f985db640aebc32cdc16a98270965aae938aa1a
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-16 23:02:20 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/Uub92rmN"))()
 
-local Window = library:AddWindow("Sync X - Cheating Tool | Legends Of Speed ⚡",
+local Window = library:AddWindow("VIP Turtle Hub V5 - Cheating Tool | Legends Of Speed ⚡",
     {
         main_color = Color3.fromRGB(170, 0, 0),
         min_size = Vector2.new(373, 433),
